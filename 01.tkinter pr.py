015f632e334d8f4dd5737bb05236997f40f48e19
Jeebanjyoti Mallik <111642123+Lifelightx@users.noreply.github.com>
2023-01-22 12:00:34 +0530

    Update 01.tkinter pr.py

--- 01.tkinter pr.py
+++ 01.tkinter pr.py
@@ -7,30 +7,39 @@ root.title("Jeeban")
 #BooleanVar, DoubleVar, IntVar,StringVar
 # The variable class has two methods get and set
 
-user_value = tk.StringVar()
-password = tk.StringVar()
-
+name = tk.StringVar()
+phone_no = tk.IntVar()
+gender = tk.StringVar()
 #creating a frame 
+frame0 = tk.Frame(root)
+frame0.pack(fill="x")
+label0 = tk.Label(frame0,text="Welcome to Python Class",font='Gerogia 15 bold')
+label0.pack(pady=50)
+
 frame = tk.Frame(root,borderwidth=3)
-frame.pack(anchor='w',padx=120,pady=130)
+frame.pack(anchor='w',padx=165,pady=130)
 
 #adding username and password label
-label1 = tk.Label(frame,text="UserName: ")
-label1.grid(row=0,column=0)
-label2 = tk.Label(frame,text="Password: ")
-label2.grid(row=1,column=0)
+label1 = tk.Label(frame,text="Name: ")
+label1.grid(row=0,column=0,padx=20)
+label2 = tk.Label(frame,text="Phone No: ")
+label2.grid(row=1,column=0,padx=20)
+label2 = tk.Label(frame,text="Gender: ")
+label2.grid(row=2,column=0,padx=20)
 #Entry Widget in which you can enter the value
-user_entry = tk.Entry(frame,textvariable=user_value)
-pass_entry = tk.Entry(frame,textvariable=password)
+user_entry = tk.Entry(frame,textvariable=name)
+pass_entry = tk.Entry(frame,textvariable=phone_no)
+gender_entry = tk.Entry(frame,textvariable=gender)
 
 user_entry.grid(row=0,column=1)
 pass_entry.grid(row=1,column=1)
+gender_entry.grid(row=2,column=1)
 
 def getvals():
-    print(user_value.get())
-    print(password.get())
+    print(name.get())
+    print(phone_no.get())
+    print(gender.get())
 
 button = tk.Button(frame,text="Submit",command=getvals,fg="white",bg='red')
-button.grid(row=2,column=1)
+button.grid(row=3,column=1)
 root.mainloop()
-
